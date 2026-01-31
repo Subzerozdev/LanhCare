e8b99f4d4e4209f52084d288881fdf28a5d25772
Subzerozdev <khanhtrantuc2004@gmail.com>
2026-01-31 08:55:09 +0700

    Fix missing List import in repositories

--- src/main/java/com/lanhcare/repository/DailyLogRepository.java
+++ src/main/java/com/lanhcare/repository/DailyLogRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface DailyLogRepository extends JpaRepository<DailyLog, Integer> {
--- src/main/java/com/lanhcare/repository/PostRepository.java
+++ src/main/java/com/lanhcare/repository/PostRepository.java
@@ -10,6 +10,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * Repository for Post entity
